6122db97c61396af48f28cdc9303b05f433bd5d7
Arvind <mehairjanarvind@gmail>
2023-12-29 21:01:06 +0100

    added new test

--- tests/e2e/fullCustomerJourney.spec.js
+++ tests/e2e/fullCustomerJourney.spec.js
@@ -9,6 +9,11 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Swag Labs/);
 });
 
+test('has url', async ({ page }) => {
+  await expect(page).toHaveURL("https://www.saucedemo.com/");
+});
+
+
 test('has the name Swag Labs on login logo', async ({ page }) => {
   // Use the correct class name for the locator
   await expect(page.locator('.login_logo')).toHaveText('Swag Labs');
